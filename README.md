4c58c74f6733cb9654c0c1a6164564ce87ddd536
FranklinY Yuan <franklinyuan0205@gmail.com>
2024-07-29 11:35:03 +1200

    init

--- README.md
+++
@@ -1,1 +0,0 @@
-Comp Sci Prooject
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,42 @@
 class_name Player extends Stats
+extends CharacterBody2D
 
 @export var Player_Mana : int = Max_Mana
+# -------------- CONSTANTS ----------
+
+const speed = 100
+
+# -------------- PHYSICS --------
+
+func _physics_process(delta):
+	pass
+	
+func player_movement(delta):
+	
+	# --------- dfs
+	
+	
+	# --------- WASD
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_down"):
+		velocity.x = 0
+		velocity.y = -speed
+	elif Input.is_action_pressed("ui_up"):
+		velocity.x = 0
+		velocity.y = speed
+	else:
+		velocity.x = 0
+		velocity.y = 0
+		
+	move_and_slide()
+		
+		
 
 var Max_Mana : int
 #initialise

a8adfbcaec9b7356fcb7e7425ecbd709db281c27
FranklinY Yuan <franklinyuan0205@gmail.com>
2024-09-06 15:07:01 +1200

    fixed force push, added more levels and made movement walk on middle of tile

--- Scripts/playercharacter.gd
+++ Scripts/playercharacter.gd
@@ -2,24 +2,22 @@ extends CharacterBody2D
 
 const speed = 100 #standard movement speed
 var current_dir = "none" #direction of movement
-var nav_active: bool  = false #is pathfinding active?
+var nav_active: bool  = false #is pathfinding active? #RETIRED
+var current_scene_name : String = ""
+var current_path_to_move = []
 #@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 
 func _ready():
 	#default animation
-	
+	current_scene_name = get_tree().current_scene.name
 	$AnimatedSprite2D.play("idle_down")
 
 func _physics_process(delta):
-	#if nav_active:
-		##var next_path_pos := navigation_agent.get_next_path_position()
-		##var direction := global_position.direction_to(next_path_pos)
-		##velocity = direction * speed
-	#else:
-		#velocity = Vector2.ZERO
+	movement_control(delta)
+	play_anim(nav_active)
 	
 	move_and_slide()
-	player_movement(delta)
+	#player_movement(delta)
 	
 	#handle mouse button inputs
 func _unhandled_input(event: InputEvent) -> void:
@@ -29,14 +27,86 @@ func _unhandled_input(event: InputEvent) -> void:
 			#navigation_agent.target_position = get_global_mouse_position()
 			nav_active = true
 			
-func bfs_move(mouse_pos) -> void:
-	var adj = GlobalUtil.load_mesh(get_tree().current_scene.name)
-	#TESTED OK
-	#///////////////////
-	var parent = [global_position.x, global_position.y]
-	var target = [mouse_pos.x, mouse_pos.y]
+func movement_control(delta):
 	
-	print(target)
+	print(current_path_to_move)
+	if current_path_to_move.size() < 1:
+		nav_active = false
+	if nav_active:
+		var current_pos = [GlobalUtil.snap_to_whole(global_position.x/16 - 0.5, 0.1) + 0.5, GlobalUtil.snap_to_whole(global_position.y/16, 0.1)]
+		#print(current_pos)
+		if GlobalUtil.hypot(current_pos, current_path_to_move.back()) == 0:
+			current_path_to_move.pop_back()
+		var next_path_pos = current_path_to_move.back()
+		if next_path_pos == null:
+			pass
+			nav_active = false
+		else:
+			next_path_pos = Vector2(next_path_pos[0]*16,next_path_pos[1]*16)
+			var direction = global_position.direction_to(next_path_pos)
+			velocity = direction * speed
+		
+	else:
+		velocity = Vector2.ZERO
+		
+func bfs_move(mouse_pos) -> void: #breadth first search guarantees shortest path in unweighted graph!
+	var adj = GlobalUtil.load_mesh(current_scene_name)
+	
+	print(adj)
+	#TESTED OK (it wasnt ok)
+	#///////////////////
+	var source = [int(roundf(global_position.x/16 - 0.5)), int(roundf(global_position.y)/16)] #round player pos to whole number
+	var target = [mouse_pos.x / 16 - 0.5, mouse_pos.y / 16 - 0.5]
+	
+	var q = [] #queue
+	var vis = {} #dict of visited nodes
+	var dist = {} #dict of distance from source
+	var prev = {} #dict of the previous node in the path from a node
+	
+	if GlobalUtil.currentSceneActiveCoords == null:
+		OS.alert("Fatal Error has occured", "ALERT")
+	
+	for coord in GlobalUtil.currentSceneActiveCoords: #init visited, dist, prev dicts
+		vis[coord] = false
+		dist[coord] = INF
+		prev[coord] = null
+		
+	#print(vis)
+		
+	q.push_back(source)
+	vis[source] = true
+	dist[source] = 0
+	
+	while q.size() != 0: #while queue not empty
+		var curr = q.pop_front()
+		#print(curr)
+		for adjnode in adj[current_scene_name][curr]:
+			if (vis[adjnode] == false):
+				#print("Aha")
+				vis[adjnode] = true
+				dist[adjnode] = dist[curr] + 1
+				prev[adjnode] = curr
+				
+				q.push_back(adjnode)
+	
+	#print(target)
+	
+	target = GlobalUtil.currentSceneTree.findNearestNeighbour(target)
+	
+	#Backtrack along prev array
+	var path = [] #REVERSED (mouse to character instead of character to mouse)
+	var node = target
+	while node != source:
+		path.append([node[0]+0.5,node[1]])
+		node = prev[node]
+	
+	current_path_to_move = path
+	
+	#print(path)
+	
+	
+	
+	#print(target)
 	
 	
 	
@@ -81,25 +151,36 @@ func player_movement(delta):
 	move_and_slide()
 	
 func play_anim(movement):
+	
+	print(velocity.x,", ", velocity.y)
+	
+	if abs(velocity.y) > abs(velocity.x):
+		if velocity.y > 0: current_dir = "down"
+		elif velocity.y < 0: current_dir = "up"
+	elif abs(velocity.x) > abs(velocity.y):
+		if velocity.x > 0: current_dir = "right"
+		elif velocity.x < 0: current_dir = "left"
+	
+
 	var dir = current_dir
 	var anim = $AnimatedSprite2D
 	if dir == "right":
-		if movement == 1:
+		if movement == true:
 			anim.play("walk_right")
-		elif movement == 0:
+		elif movement == false:
 			anim.play("idle_right")
-	if dir == "left":
-		if movement == 1:
+	elif dir == "left":
+		if movement == true:
 			anim.play("walk_left")
-		elif movement == 0:
+		elif movement == false:
 			anim.play("idle_left")
-	if dir == "up":
-		if movement == 1:
+	elif dir == "up":
+		if movement == true:
 			anim.play("walk_up")
-		elif movement == 0:
+		elif movement == false:
 			anim.play("idle_up")
-	if dir == "down":
-		if movement == 1:
+	elif dir == "down":
+		if movement == true:
 			anim.play("walk_down")
-		elif movement == 0:
+		elif movement == false:
 			anim.play("idle_down")
--- Scripts/sceneloader.gd
+++ Scripts/sceneloader.gd
@@ -2,9 +2,51 @@ extends Node
 
 @onready var tile_map: TileMap = $"../TileMap"
 # Called when the node enters the scene tree for the first time.
+
+func create_balanced_ktree(tree,pointsAr) -> GlobalUtil.kTree: #pointsAr should be already X-sorted
+	#print(pointsAr)
+	#print(pointsAr.size())
+	if pointsAr.size() == 0:
+		return tree
+	if pointsAr.size() < 2:
+		tree.insertNode(pointsAr[0])
+		return tree
+		
+	#print(pointsAr)
+		#
+	tree.insertNode(pointsAr[int(pointsAr.size()/2)])
+	var leftHalf = pointsAr.slice(0,int(pointsAr.size()/2)) #split in half around median
+	var rightHalf = pointsAr.slice(int(pointsAr.size()/2),pointsAr.size())
+	
+	#print("left:", leftHalf)
+	#print("right:", rightHalf)
+	
+	tree = create_balanced_ktree(tree, leftHalf)
+	tree = create_balanced_ktree(tree, rightHalf)
+	return tree
+
 func _ready():
-	GlobalUtil.write_mesh_json(tile_map.get_used_cells(0),get_tree().current_scene.name)
+	#get cells and create balanced K-tree
+	var cell_points = tile_map.get_used_cells(0)
+	cell_points = GlobalUtil.vector_to_array(cell_points)
+	
+	
+	GlobalUtil.write_mesh_json(cell_points,get_tree().current_scene.name)
+	var tree = GlobalUtil.kTree.new()
+	
+	tree = create_balanced_ktree(tree,cell_points)
+	
+	#for x in cell_points:
+		#print(x)
+		#
+	#print("#######")
+	#tree.traverse(tree.root)
+	
+	GlobalUtil.setCurrentSceneCoords(cell_points)
+	GlobalUtil.setCurrentSceneTree(tree)
+	print("scene load done")
 	
 	#var bruh = GlobalUtil.load_mesh(get_tree().current_scene.name)
 	#print(bruh)
+
 
